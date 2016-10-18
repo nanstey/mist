e75adfe3d4b03a81ebf0170899011cfbef3c4600
Noel Anstey <anstey.ng@gmail.com>
2016-10-18 14:10:55 -0700

    uvicmist init

---
+++ Desktop/uvicmist/uvicmist/music_test/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'music_test'
+urlpatterns = [
+    # ex: /music_test/
+    # url(r'^$', views.IndexView.as_view(), name='index'),
+]
