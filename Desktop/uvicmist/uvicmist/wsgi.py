0b4409be6cbcf5d0e1f341fcd3a6150b6973a107
Noel Anstey <anstey.ng@gmail.com>
2016-10-18 17:03:39 -0700

    fix wsgi

--- Desktop/uvicmist/uvicmist/wsgi.py
+++ Desktop/uvicmist/uvicmist/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "interval_singing.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uvicmist.settings")
 
 application = get_wsgi_application()
